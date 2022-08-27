4382fedad32c987d6a13990155bb87722063d4a2
Christian Rodriguez <chrisrdz99@hotmail.com>
2022-08-27 18:37:57 -0500

    Added user input to the api endpoint, and also added cors, now the images are served as an url to the front-end

--- server/functions/saveAd.js
+++ server/functions/saveAd.js
@@ -1,81 +1,78 @@
 const puppeteer = require('puppeteer');
 const { v4: uuidV4 } = require('uuid');
 
-const saveAd = async() => {
-  const browser = await puppeteer.launch({headless: false, defaultViewport: {'height': 1080, 'width': 1920}, timeout: 60000});
+const saveAd = async(price, description) => {
+  async function changeDropdown(anchorTag, listTag) {
+    await page.$eval(anchorTag, elem => elem.click());
+    await new Promise(r => setTimeout(r, 500));
+    await page.$eval(listTag, elem => elem.click());
+    await new Promise(r => setTimeout(r, 500));
+  }
+  const browser = await puppeteer.launch({headless: true, defaultViewport: {'height': 1080, 'width': 1920}, timeout: 60000});
 
   const page = await browser.newPage();
   await page.goto('https://www.seminuevos.com/login');
-  // await page.click('a[data-activates="slide-out"]');
-  // await Promise.all([page.click('a[href="/login"]'), page.waitForNavigation()]);
 
   await page.type('#email_login', process.env.LOGIN_EMAIL, {delay: 100});
   await page.type('#password_login', process.env.LOGIN_PASSWORD, {delay: 100});
   await Promise.all([page.click('button[type="submit"]'), page.waitForNavigation()]);
 
   await Promise.all([page.click('a[href="/wizard?f_dealer_id=-1"]'), page.waitForNavigation()]);
-  // const typeDropdown = await page.$eval('#dropdown_types');
-  // console.log(typeDropdown);
-  // await page.click('div#dropdown_types', {delay: 100});
+
   await new Promise(r => setTimeout(r, 3000));
-  await page.$eval('a[data-activates="dropdown_types"]', elem => elem.click());
-  await new Promise(r => setTimeout(r, 500));
-  await page.$eval('li[data-content="autos"] a[href="#"]', elem => elem.click());
 
-  await page.$eval('a[data-activates="dropdown_brands"]', elem => elem.click());
-  await new Promise(r => setTimeout(r, 500));
-  await page.$eval('li[data-content="acura"] a[href="#"]', elem => elem.click());
-
-  await page.$eval('a[data-activates="dropdown_models"]', elem => elem.click());
-  await new Promise(r => setTimeout(r, 500));
-  await page.$eval('li[data-content="ilx"] a[href="#"]', elem => elem.click());
-
-  await page.$eval('a[data-activates="dropdown_subtypes"]', elem => elem.click());
-  await new Promise(r => setTimeout(r, 500));
-  await page.$eval('li[data-content="sedan"] a[href="#"]', elem => elem.click());
-
-  await page.$eval('a[data-activates="dropdown_years"]', elem => elem.click());
-  await new Promise(r => setTimeout(r, 500));
-  await page.$eval('li[data-content="2018"] a[href="#"]', elem => elem.click());
-  
-  await page.$eval('a[data-activates="dropdown_provinces"]', elem => elem.click());
-  await new Promise(r => setTimeout(r, 500));
-  await page.$eval('li[data-content="nuevo leon"] a[href="#"]', elem => elem.click());
-
-  await page.$eval('a[data-activates="dropdown_cities"]', elem => elem.click());
-  await new Promise(r => setTimeout(r, 500));
-  await page.$eval('li[data-content="monterrey"] a[href="#"]', elem => elem.click());
+  await changeDropdown('a[data-activates="dropdown_types"]', 'li[data-content="autos"] a[href="#"]');
+  await changeDropdown('a[data-activates="dropdown_brands"]', 'li[data-content="acura"] a[href="#"]');
+  await changeDropdown('a[data-activates="dropdown_models"]', 'li[data-content="ilx"] a[href="#"]')
+  await changeDropdown('a[data-activates="dropdown_subtypes"]', 'li[data-content="sedan"] a[href="#"]')
+  await changeDropdown('a[data-activates="dropdown_years"]', 'li[data-content="2018"] a[href="#"]')
+  await changeDropdown('a[data-activates="dropdown_provinces"]', 'li[data-content="nuevo leon"] a[href="#"]')
+  await changeDropdown('a[data-activates="dropdown_cities"]', 'li[data-content="monterrey"] a[href="#"]')
 
   await page.type('#input_recorrido', '20000', { delay: 100 });
-  await page.type('#input_precio', '200000', { delay: 100 });
+  await page.type('#input_precio', price, { delay: 100 });
 
   await Promise.all([page.click('button[class="next-button"]'), page.waitForNavigation()]);
 
   await new Promise(r => setTimeout(r, 3000));
 
-  await page.type('#input_text_area_review', 'Esto es un anuncio de tipo testing, y las imagenes son tomadas de google, todos los derechos de las imagenes son reservados', {delay: 50});
+  await page.type('#input_text_area_review', description, {delay: 50});
 
   const files = ['../imagenes_prueba/acura_img1.jpg', '../imagenes_prueba/acura_img2.jpg', '../imagenes_prueba/acura_img3.jpg'];
   const inputFile = await page.$('input[type="file"]#Uploader');
   await inputFile.uploadFile(...files);
 
+  await new Promise(r => setTimeout(r, 5000));
+
   await Promise.all([page.click('button[class="next-button"]'), page.waitForNavigation()]);
 
-  await page.click('a[data-tooltip="Publicaciones"]');
+  await new Promise(r => setTimeout(r, 5000));
 
-  const newAd = await page.$$eval('#sidebar-my-vehicles div div div div.article', elem => elem.pop());
+  const url = await page.evaluate(() => document.location.href);
 
-  await Promise.all([newAd.querySelector('div div.features.clearfix a').click(), page.waitForNavigation()]);
+  await Promise.all([page.goto(url.slice(0, url.lastIndexOf('/'))), page.waitForNavigation()])
+
+  await new Promise(r => setTimeout(r, 5000));
 
   const newID = uuidV4();
 
-  await page.screenshot({path: `/images/${newID}.png`});
+  await page.screenshot({path: `./images/${newID}.png`});
 
   await browser.close();
 
   return newID;
 }
 
+const imageTest = async() => {
+  const browser = await puppeteer.launch({headless: false});
+
+  const page = await browser.newPage();
+
+  await page.goto('https://www.google.com/');
+
+  await page.screenshot({path: './images/test.png'});
+}
+
 module.exports = {
-  saveAd
+  saveAd, imageTest
 }
--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
 require('dotenv').config();
 const express = require('express');
-const { saveAd } = require('./functions/saveAd');
+const { saveAd, imageTest } = require('./functions/saveAd');
 const app = express();
+const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
+app.get('/', async(req, res) => {
+  await imageTest();
   res.send('Hello world');
 });
 
 app.post('/save-ad', async(req, res) => {
-  const id = await saveAd();
-  res.sendFile(`/images/${id}.png`);
-})
+  try {
+    const id = await saveAd(req.body.price, req.body.description);
+    res.json({error: false, url: `http://localhost:${PORT}/images/${id}.png`});
+  } catch (error) {
+    res.json({error: true, msg: error});
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
