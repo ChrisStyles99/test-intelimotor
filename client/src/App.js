666cd12fc438624db52d85d5dafb0e95f02f2b20
Christian Rodriguez <chrisrdz99@hotmail.com>
2022-08-27 20:29:45 -0500

    Añadido formulario en el front-end con los pasos solicitados, y formulario con validaciones en el cliente

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Typography } from "@mui/material";
+import { useState } from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Form from "./components/Form";
+import Navbar from "./components/Navbar";
 
 function App() {
+
+  const [imageURL, setImageURL] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <Navbar />
+      <Typography variant="h1" textAlign={"center"}>Añadir anuncio</Typography>
+      <Typography variant="body1" textAlign={"center"}>
+        Este formulario sirve para poder añadir anuncios en la pagina seminuevos.com, con esto, se hace mas facil la tarea de añadir anuncios, solamente se tiene
+        que añadir el precio del carro del anuncio, y una descripción de las caracteristicas que tenga el auto.
+      </Typography>
+      <Box marginTop={4}>
+        <Form setImageURL={setImageURL} />
+      </Box>
+      {imageURL && <Box display={"flex"} marginTop={4} justifyContent={"center"} overflow={"auto"}>
+        <img src={imageURL} alt="Screenshot del anuncio publicado" />
+      </Box>}
     </div>
   );
 }
---
+++ client/src/components/Form.js
@@ -0,0 +1,64 @@
+import { Button, Card, CardContent, CircularProgress, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+
+import { useForm } from '../hooks/useForm';
+import { baseInstance } from '../utils/axiosInstance';
+
+function Form({ setImageURL }) {
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { formData, handleChange, resetForm } = useForm({price: '', description: ''});
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    toast('Se está publicando el anuncio', {
+      autoClose: true, 
+      position: 'top-right',
+      theme: 'colored',
+      type: 'info'
+    });
+    setIsSubmitting(true);
+    const data = await baseInstance.post('/save-ad', {
+      price: formData.price,
+      description: formData.description
+    });
+    if(data.data.error) {
+      setImageURL(null);
+      toast('Hubo un error al publicar el anuncio :(', {
+        autoClose: true, 
+        position: 'top-right',
+        theme: 'colored',
+        type: 'error'
+      });
+    } else {
+      setImageURL(data.data.url);
+      toast('Anuncio publicado con exito', {
+        autoClose: true, 
+        position: 'top-right',
+        theme: 'colored',
+        type: 'success'
+      });
+    }
+    setIsSubmitting(false);
+    resetForm();
+  }
+
+  return (
+    <Card sx={{ maxWidth: 600, margin: 'auto' }}>
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          <Typography variant='h3' textAlign={"center"}>Formulario</Typography>
+          <TextField inputProps={{min: 1000}} name="price" value={formData.price} onChange={handleChange} fullWidth margin='normal' required type="number" label="Precio del carro" 
+            placeholder="El precio del auto del anuncio"/>
+          <TextField name="description" inputProps={{minLength: 10}} value={formData.description} onChange={handleChange} fullWidth margin='normal' required type="text" minRows={5} multiline 
+            label="Descripción del anuncio (Mayor a 10 caracteres)" placeholder="La descripción del auto del anuncio"/>
+          {!isSubmitting ? <Button type='submit' variant='contained' color='secondary' fullWidth >Publicar</Button> : <CircularProgress />}
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Form
